65bdf3a6def832d5fdd2e1d9e53c95286f9dc2ae
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-12 14:28:16 -0400

    updated findEmployees function in db/index.js

--- db/index.js
+++ db/index.js
@@ -15,11 +15,10 @@ class Data {
     }
 
     findEmployees(){
-       // employee data: employee ids, first, last, job titles, department name, salaries, and managers full name that employees report to (look into CONCAT() mysql)
-       //employee table: employee.id, employee.first_name, employee.last_name
-       // role table: role.title, role.salary, 
-        return this.connection.promise().query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary FROM employee left join role on employee.role_id = role.id;');
-    }
+        // employee data: employee ids, first, last, job titles, department name, salaries, and managers full name that employees report to (look into CONCAT() mysql)
+      
+         return this.connection.promise().query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name as department_name, CONCAT(first_name, " ", last_name) AS reporting_manager FROM employee left join role on employee.role_id = role.id left JOIN department on department.id = role.department_id;');
+     }
 }
 
 module.exports = new Data(connection)
